04cacd6d5a196ab2c0e010a39bb05db92898946c
Gulnaz Nasrtdinova <gulnaz0206@yandex.ru>
2023-08-01 18:16:55 +0500

    Update validation.js

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -19,8 +19,6 @@ const authValidation = celebrate({
 const regValidation = celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
-    // about: Joi.string().min(2).max(30),
-    // avatar: Joi.string().custom(validateUrl),
     email: Joi.string().required().email(),
     password: Joi.string().required(),
   }),
